148c0bccd5389c0fc549e88973abd9b33f537383
stfrigerio <stefri3@gmail.com>
2024-10-02 04:41:36 +0200

    Update build mobile.bat

--- build mobile.bat
+++ build mobile.bat
@@ -1,2 +1,2 @@
 :: Start the server
-start cmd /k "cd /d %~dp0packages/mobile/android && gradlew assembleRelease && echo. && echo Now copy paste this once your phone is correctly connected to the computer: adb install app\build\outputs\apk\release\app-release.apk"
+start cmd /k "cd /d %~dp0packages/mobile/android && gradlew clean && gradlew assembleRelease"
